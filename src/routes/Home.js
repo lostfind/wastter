36961f67853ccc091b10f422e79d256c82eed3aa
lostfind <kpdzzang@gmail.com>
2021-03-14 21:21:32 +0900

    wasteed to firestore

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,33 @@
-import React from "react";
+import { dbService } from "fbase";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [waste, setWaste] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("wastes").add({
+      waste,
+      createAt: Date.now(),
+    });
+    setWaste("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setWaste(value);
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        value={waste}
+        onChange={onChange}
+        type="text"
+        placeholder="Input your waste"
+        maxlength={120}
+      />
+      <input type="submit" value="Wastteed" />
+    </form>
+  );
+};
 export default Home;
